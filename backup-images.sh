93c7de35992421233b245a4a72e63155fe2b10f6
Olatife <Boluwatife.E.O@outlook.com>
2025-08-16 13:44:15 +0200

    Delete backup-images.sh

--- backup-images.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p image-backup
-find . -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.gif" -o -iname "*.svg" -o -iname "*.bmp" -o -iname "*.webp" \) -exec cp --parents {} image-backup/ \;
